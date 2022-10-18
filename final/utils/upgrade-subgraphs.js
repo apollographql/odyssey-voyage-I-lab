68ffb14e12f13dc692c9ef610d496af9b7377a9c
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2022-10-18 13:17:06 -0600

    revert upgrade-subgraphs specification of version

--- final/utils/upgrade-subgraphs.js
+++ final/utils/upgrade-subgraphs.js
@@ -4,19 +4,19 @@ const path = require('path');
 concurrently(
   [
     {
-      command: 'npm install @apollo/subgraph@2.1.3 graphql@16.6.0',
+      command: 'npm install @apollo/subgraph@latest-2',
       name: 'locations',
       cwd: path.resolve(__dirname, '../subgraph-locations'),
       prefixColor: 'blue',
     },
     {
-      command: 'npm install @apollo/subgraph@2.1.3 graphql@16.6.0',
+      command: 'npm install @apollo/subgraph@latest-2',
       name: 'activities',
       cwd: path.resolve(__dirname, '../subgraph-activities'),
       prefixColor: 'magenta',
     },
     {
-      command: 'npm install @apollo/subgraph@2.1.3 graphql@16.6.0',
+      command: 'npm install @apollo/subgraph@latest-2',
       name: 'reviews',
       cwd: path.resolve(__dirname, '../subgraph-reviews'),
       prefixColor: 'green',
